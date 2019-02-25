b7ea64db4cfe5d7fc2da90e68b1544a393777927
Periklis_ts <ptsirostsib@gmail.com>
2019-02-25 18:22:12 +0200

    Update predictpbpk.R

--- R/predictpbpk.R
+++ R/predictpbpk.R
@@ -23,9 +23,9 @@ predictpbpk <- function(dataset, rawModel, additionalInfo){
   # predFeat <- additionalInfo$predictedFeatures[1][[1]]
   predFeat <- additionalInfo$predictedFeatures
 
-  initial_concentration = rep(0, n_comp)
-  for(i in 1:length(initial_concentration)){
-    con = paste("y", i, sep="")
+  init_comp = additionalInfo$comp
+  for(i in comp)){
+    con = paste("C0",i , sep="")
     initial_concentration[i] = df[[con]]
   }
   weight = df$weight
@@ -37,22 +37,22 @@ predictpbpk <- function(dataset, rawModel, additionalInfo){
   sample_time <- c(0, 5/60, 0.25, 0.5, 0.75, 1, 1.5, 2, 3, 4, 6, 8, 10, 12, 24, 36, 48, 72) # in hours
   solution <- ode(y = initial_concentration, times = sample_time, func = odemodel, parms = params)
 
-  comp_names <- rep(0, n_comp + 1)
-  comp_short <- rep(0, n_comp + 1)
-  pred_names <- rep(0, n_comp + 1)
-  for(i in 1:length(comp_names)){
-    comp_names[i] <- additionalInfo$predictedFeatures[[i]]
-    comp_short[i] <- as.integer(strsplit(additionalInfo$predictedFeatures[[i]], "_")[[1]])
-    pred_names[i] <- names(additionalInfo$predictedFeatures)[i]
-  }
-  comp_all <- data.frame(cbind(comp_names, comp_short, pred_names))
-  comp_all_s <- comp_all[order(comp_short),][,1]
+  #comp_names <- rep(0, n_comp + 1)
+  #comp_short <- rep(0, n_comp + 1)
+  #pred_names <- rep(0, n_comp + 1)
+  #for(i in 1:length(comp_names)){
+  #  comp_names[i] <- additionalInfo$predictedFeatures[[i]]
+  #  comp_short[i] <- as.integer(strsplit(additionalInfo$predictedFeatures[[i]], "_")[[1]])
+  #  pred_names[i] <- names(additionalInfo$predictedFeatures)[i]
+  #}
+  #comp_all <- data.frame(cbind(comp_names, comp_short, pred_names))
+  #comp_all_s <- comp_all[order(comp_short),][,1]
 
 
 
   for(i in 1:dim(solution)[1]){
     prediction<- data.frame(t(solution[i,]))
-    colnames(prediction)<- comp_all_s
+    colnames(prediction)<- c("time", comp)
     if(i==1){lh_preds<- list(unbox(prediction))
     }else{
       lh_preds[[i]]<- unbox(prediction)
